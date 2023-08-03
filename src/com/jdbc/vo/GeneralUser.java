dc1f4c9ccbee285685fbec5bd64079e5719fc728
daeun0220 <daeunkim1284@gmail.com>
2023-08-03 17:19:32 +0900

    GeneralUser 수정

--- src/com/jdbc/vo/GeneralUser.java
+++ src/com/jdbc/vo/GeneralUser.java
@@ -1,13 +1,23 @@
 package com.jdbc.vo;
 
+import java.util.ArrayList;
+
 public class GeneralUser extends User{
 	private String nickname;
+	private ArrayList<Review> portfolio;
 	
 	public GeneralUser() {}
 
 	public GeneralUser(String userId, int admin, String nickname) {
 		super(userId, admin);
 		this.nickname = nickname;
+	}
+	
+
+	public GeneralUser(String userId, int admin, String nickname, ArrayList<Review> portfolio) {
+		super(userId, admin);
+		this.nickname = nickname;
+		this.portfolio = portfolio;
 	}
 
 	public String getNickname() {
@@ -18,10 +28,20 @@ public class GeneralUser extends User{
 		this.nickname = nickname;
 	}
 
+	public ArrayList<Review> getPortfolio() {
+		return portfolio;
+	}
+
+	public void setPortfolio(ArrayList<Review> portfolio) {
+		this.portfolio = portfolio;
+	}
+
 	@Override
 	public String toString() {
-		return "GeneralUser [nickname=" + nickname + "]"+super.toString();
+		return "GeneralUser [nickname=" + nickname + ", portfolio=" + portfolio + "]"+super.toString();
 	}
+
+	
 	
 
 }
